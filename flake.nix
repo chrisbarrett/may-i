827af57988c2fbf2aacf8a25659ccbb6ae2c7cab
Chris Barrett <chris@walrus.cool>
2026-02-15 12:41:06 +1300

    Add build output for flake

--- flake.nix
+++ flake.nix
@@ -1,12 +1,16 @@
 {
-  description = "may-i - shell command authorization evaluator";
+  description = "may-i - claude-code Bash permissions checker";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -24,6 +28,13 @@
           shellHook = ''
             prek install
           '';
+        };
+
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "may-i";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
         };
       });
 }
