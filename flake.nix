f0b2c72434dacaa7af0a1438e832f7da32f6b258
Chris Barrett <chris@walrus.cool>
2026-03-20 20:50:11 +1300

    Update flake

--- flake.nix
+++ flake.nix
@@ -22,96 +22,97 @@
     };
   };
 
-  outputs = inputs@{ advisory-db, flake-parts, self, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = [
-      "aarch64-darwin"
-      "aarch64-linux"
-      "x86_64-linux"
-    ];
+  outputs = inputs@{ advisory-db, flake-parts, self, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
 
-    perSystem = { self', system, ... }:
-      let
-        pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [
-            inputs.rust-overlay.overlays.default
-            inputs.cargo-affected.overlays.default
-          ];
-        };
+      perSystem = { self', system, ... }:
+        let
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              inputs.rust-overlay.overlays.default
+              inputs.cargo-affected.overlays.default
+            ];
+          };
 
-        craneLib = (inputs.crane.mkLib pkgs).overrideToolchain (p:
-          p.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml
-        );
+          craneLib = (inputs.crane.mkLib pkgs).overrideToolchain (p:
+            p.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml
+          );
 
-        # Include non-Rust files needed for the build
-        extraFileFilter = path: type:
-          (pkgs.lib.hasSuffix ".lisp" path) ||
-          (pkgs.lib.hasSuffix ".snap" path);
+          # Include non-Rust files needed for the build
+          extraFileFilter = path: type:
+            (pkgs.lib.hasSuffix ".lisp" path) ||
+            (pkgs.lib.hasSuffix ".snap" path);
 
-        src = pkgs.lib.cleanSourceWith {
-          src = ./.;
-          filter = path: type:
-            (extraFileFilter path type) ||
-            (craneLib.filterCargoSources path type);
-        };
+          src = pkgs.lib.cleanSourceWith {
+            src = ./.;
+            filter = path: type:
+              (extraFileFilter path type) ||
+              (craneLib.filterCargoSources path type);
+          };
 
-        commonArgs = {
-          inherit src;
-          strictDeps = true;
-        };
+          commonArgs = {
+            inherit src;
+            strictDeps = true;
+          };
 
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-        may-i = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
-          doCheck = false; # Run via cargo-nextest checks.
-        });
-      in
-      {
-        formatter = pkgs.nixpkgs-fmt;
-
-        apps.default = {
-          type = "app";
-          program = "${may-i}/bin/may-i";
-        };
-        packages.default = may-i;
-
-        checks = {
-          inherit may-i;
-
-          clippy = craneLib.cargoClippy (commonArgs // {
+          may-i = craneLib.buildPackage (commonArgs // {
             inherit cargoArtifacts;
-            cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            doCheck = false; # Run via cargo-nextest checks.
           });
+        in
+        {
+          formatter = pkgs.nixpkgs-fmt;
 
-          fmt = craneLib.cargoFmt { inherit src; };
-          audit = craneLib.cargoAudit { inherit src advisory-db; };
+          apps.default = {
+            type = "app";
+            program = "${may-i}/bin/may-i";
+          };
+          packages.default = may-i;
 
-          nextest = craneLib.cargoNextest (commonArgs // {
-            inherit cargoArtifacts;
-            partitions = 1;
-            partitionType = "count";
-            cargoNextestPartitionsExtraArgs = "--no-tests=pass";
-          });
+          checks = {
+            inherit may-i;
+
+            clippy = craneLib.cargoClippy (commonArgs // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            });
+
+            fmt = craneLib.cargoFmt { inherit src; };
+            audit = craneLib.cargoAudit { inherit src advisory-db; };
+
+            nextest = craneLib.cargoNextest (commonArgs // {
+              inherit cargoArtifacts;
+              partitions = 1;
+              partitionType = "count";
+              cargoNextestPartitionsExtraArgs = "--no-tests=pass";
+            });
+          };
+
+          devShells.default = craneLib.devShell {
+            checks = self'.checks;
+
+            packages = with pkgs; [
+              cargo-affected
+              cargo-tarpaulin
+              prek
+            ];
+
+            shellHook = ''
+              prek install
+            '';
+          };
         };
 
-        devShells.default = craneLib.devShell {
-          checks = self'.checks;
-
-          packages = with pkgs; [
-            cargo-affected
-            cargo-tarpaulin
-            prek
-          ];
-
-          shellHook = ''
-            prek install
-          '';
-        };
+      flake.overlays.default = final: prev: {
+        may-i = self.packages.${prev.system}.default;
       };
-
-    flake.overlays.default = final: prev: {
-      may-i = self.packages.${prev.system}.default;
     };
-  };
 }
